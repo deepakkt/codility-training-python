5662d6cfd411b8eaa3fe4d75d4fc23767670fbab
Deepak <deepak.kumaran@aurea.com>
2021-06-22 11:32:06 +0530

    7.3

---
+++ 007-stacks-and-queues/codility-stacks-and-queues-3-nesting.py
@@ -0,0 +1,10 @@
+## the exact solution for 7.1
+## works with a 100% result
+## here as well
+## use the solution
+## https://github.com/deepakkt/codility-training-python/blob/main/007-stacks-and-queues/codility-stacks-and-queues-1-brackets.py
+
+## surprised this is presented as a separate problem
+##
+
+pass
